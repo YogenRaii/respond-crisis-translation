a5e2a1f44d1158b22d001541b98ac806300fe12b
unknown <jonathanf@EXPERIMENT.LOCAL>
2021-02-20 17:58:45 +0200

    Now the type of speciality remains the same on the onboarding page but is summarized in the DB.

--- src/assets/lists/experienceLevel.js
+++ src/assets/lists/experienceLevel.js
@@ -1,8 +1,26 @@
 export default [
-    'I am a native speaker',
-    'professional medical translation',
-    'professional legal translation',
-    'professional immigration translation',
-    'have worked with immigration related agencies',
-    'experienced volunteer'
-]
+  {
+    description: "I am a native speaker",
+    skill: "Native",
+  },
+  {
+    description: "professional medical translation",
+    skill: "Medical",
+  },
+  {
+    description: "professional legal translation",
+    skill: "Legal",
+  },
+  {
+    description: "professional immigration translation",
+    skill: "Immigration",
+  },
+  {
+    description: "have worked with immigration related agencies",
+    skill: "Agencies",
+  },
+  {
+    description: "experienced volunteer",
+    skill: "Volunteer",
+  },
+];
