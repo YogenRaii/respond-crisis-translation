fd622d0cd233433d43bd80555587648f9a2592eb
Yogen Rai <yogen.rai.992@gmail.com>
2021-03-08 11:19:30 -0600

    Complete onboarding application with approve or reject Resolved #26

--- src/services/TranslatorService.js
+++ src/services/TranslatorService.js
@@ -3,3 +3,11 @@ import { db } from '../firebase'
 export const getTranslators = (status) => {
   return db.collection('translators').where('status', '==', status).get()
 }
+
+export const updateStatus = (uid, status) => {
+  return db.collection('translators').doc(uid).update({status})
+}
+
+export const updateRole = (uid, role) => {
+  return db.collection('translators').doc(uid).update({role})
+}
