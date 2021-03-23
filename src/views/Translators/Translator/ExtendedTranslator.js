64c5290a1415796a33a8d6fd3756c5bb77039949
unknown <jonathanf@EXPERIMENT.LOCAL>
2021-03-24 00:12:49 +0200

    Break Translators into several components.

---
+++ src/views/Translators/Translator/ExtendedTranslator.js
@@ -0,0 +1,116 @@
+import React from "react";
+
+export default ({ onChange, updateTranslator, role, onboard, i }) => {
+  return (
+    <>
+      <div uk-grid="">
+        <div className="uk-width-auto">
+          <p>
+            <b>Email</b>
+          </p>
+          <p>{onboard.email}</p>
+        </div>
+        <div className="uk-width-auto">
+          <p>
+            <b>Language Supports</b>
+          </p>
+          {onboard.languages?.map((language, b) => (
+            <p
+              key={
+                onboard.first_name +
+                onboard.last_name +
+                language.from_langauge +
+                language.to_language +
+                " language_support " +
+                i +
+                " " +
+                b
+              }
+            >
+              {language.from_langauge} to {language.to_language}
+            </p>
+          ))}
+        </div>
+        <div className="uk-width-auto">
+          <p>
+            <b>Oral Translation</b>
+          </p>
+          {onboard.languages?.map((language, b) =>
+            language.oral ? (
+              <p
+                key={
+                  onboard.first_name +
+                  onboard.last_name +
+                  language.from_langauge +
+                  language.to_language +
+                  " oral translation " +
+                  i +
+                  " " +
+                  b
+                }
+              >
+                {language.from_langauge} to {language.to_language}
+              </p>
+            ) : (
+              ""
+            )
+          )}
+        </div>
+        <div className="uk-width-auto">
+          <p>
+            <b>Sources</b>
+          </p>
+          {onboard.found_us?.map((sources, p) => (
+            <p
+              key={
+                onboard.first_name +
+                onboard.last_name +
+                sources +
+                " sources " +
+                i +
+                " " +
+                p
+              }
+            >
+              {sources}
+            </p>
+          ))}
+        </div>
+        <div className="uk-width-auto">{onboard.notes}</div>
+      </div>
+      <div className="uk-clearfix">
+        <div className="uk-float-left">
+          <button className="uk-button uk-button-danger">Deactivate</button>
+        </div>
+        <div className="uk-float-right">
+          {onboard.role === "admin" && role === "super_admin" ? (
+            <button
+              className="uk-button"
+              style={{ marginRight: "5px" }}
+              onClick={() => updateTranslator(onboard.id, "super_admin")}
+            >
+              Promote to Super Admin
+            </button>
+          ) : !onboard.role ? (
+            <button
+              className="uk-button"
+              style={{ marginRight: "5px" }}
+              onClick={() => updateTranslator(onboard.id, "admin")}
+            >
+              Promote to Admin
+            </button>
+          ) : (
+            <span></span>
+          )}
+          <button
+            className="uk-button uk-button-primary"
+            style={{ marginRight: "5px" }}
+          >
+            Records
+          </button>
+          <button className="uk-button uk-button-primary">Edit</button>
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/views/Translators/Translator/Translator.js
@@ -0,0 +1,84 @@
+import React from "react";
+import formatDate from "../../../assets/helpers/formatDate";
+import lang_short from "../../../assets/lists/langShort";
+import cases from "../../../assets/lists/Cases";
+import AssignTask from "../../../components/AssignTask/AssignTask";
+import ExtendedTranslator from "./ExtendedTranslator";
+
+export default ({ onChange, updateTranslator, role, onboard, i, show }) => {
+  return (
+    <React.Fragment key={onboard.first_name + onboard.last_name + " " + i}>
+      <tr style={{ cursor: "pointer" }}>
+        <td onClick={onChange}>
+          {onboard.first_name} {onboard.last_name} (3)
+        </td>
+        <td onClick={onChange}>
+          {onboard.languages?.map((language, y) => (
+            <span
+              className="uk-label"
+              key={
+                onboard.first_name +
+                onboard.last_name +
+                language.from_langauge +
+                language.to_langauge +
+                " language(s) " +
+                i +
+                " " +
+                y
+              }
+            >
+              {lang_short[language.from_langauge]} &#9658;{" "}
+              {lang_short[language.to_language]}
+            </span>
+          ))}
+        </td>
+        <td onClick={onChange}>{formatDate(onboard.date_accepted)}</td>
+        <td onClick={onChange}>{formatDate(onboard.date_accepted)}</td>
+        <td onClick={onChange}>4/5</td>
+        <td onClick={onChange}>
+          {onboard.specialities?.map((special, z) => (
+            <span
+              key={
+                onboard.first_name +
+                onboard.last_name +
+                special +
+                " " +
+                i +
+                " " +
+                z
+              }
+            >
+              {special},
+            </span>
+          ))}
+        </td>
+        <td onClick={onChange}>{onboard.notes}</td>
+        <td>
+          <AssignTask
+            first_name={onboard.first_name}
+            last_name={onboard.last_name}
+            task_in_progress={5}
+            languages={onboard.languages}
+            tasks={cases}
+          />
+        </td>
+      </tr>
+      <tr
+        style={{
+          display: show ? "" : "none",
+          borderTop: "none",
+        }}
+      >
+        <td colSpan={8}>
+          <ExtendedTranslator
+            onChange={onChange}
+            updateTranslator={updateTranslator}
+            role={role}
+            onboard={onboard}
+            i={i}
+          />
+        </td>
+      </tr>
+    </React.Fragment>
+  );
+};
